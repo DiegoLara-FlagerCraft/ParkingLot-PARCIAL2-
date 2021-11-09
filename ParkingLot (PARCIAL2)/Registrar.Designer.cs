656891ac4479533a1f9dd59a5cf4fe6b77746013
DiegoLara-FlagerCraft <laramnbvo123@gmail.com>
2021-11-08 20:54:19 -0500

    Progreso

--- ParkingLot (PARCIAL2)/Registrar.Designer.cs
+++ ParkingLot (PARCIAL2)/Registrar.Designer.cs
@@ -34,13 +34,24 @@ namespace ParkingLot__PARCIAL2_
             this.PicBoxMarco6 = new System.Windows.Forms.PictureBox();
             this.PicBoxMarco7 = new System.Windows.Forms.PictureBox();
             this.PicBoxMarco8 = new System.Windows.Forms.PictureBox();
-            this.LblRegistrar = new System.Windows.Forms.Label();
+            this.LblRegistrarV = new System.Windows.Forms.Label();
             this.LblTipoV = new System.Windows.Forms.Label();
             this.ComBoxTipoV = new System.Windows.Forms.ComboBox();
             this.LblPlacaV = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxtBoxPlacaV = new System.Windows.Forms.TextBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.LblMarcaV = new System.Windows.Forms.Label();
+            this.TxtBoxMarcaV = new System.Windows.Forms.TextBox();
+            this.LblNumeroI = new System.Windows.Forms.Label();
+            this.TxtBoxNumI = new System.Windows.Forms.TextBox();
+            this.LblSexo = new System.Windows.Forms.Label();
+            this.ComBoxSexo = new System.Windows.Forms.ComboBox();
+            this.LblAfiliacion = new System.Windows.Forms.Label();
+            this.ComBoxAfiliacion = new System.Windows.Forms.ComboBox();
+            this.BtnRegistrarV = new System.Windows.Forms.Button();
+            this.BtnBorrarDatos = new System.Windows.Forms.Button();
+            this.BtnVolver = new System.Windows.Forms.Button();
+            this.LblRegistrarC = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.PicBoxMarco5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PicBoxMarco6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PicBoxMarco7)).BeginInit();
@@ -83,17 +94,17 @@ namespace ParkingLot__PARCIAL2_
             this.PicBoxMarco8.TabIndex = 8;
             this.PicBoxMarco8.TabStop = false;
             // 
-            // LblRegistrar
+            // LblRegistrarV
             // 
-            this.LblRegistrar.AutoSize = true;
-            this.LblRegistrar.BackColor = System.Drawing.Color.Transparent;
-            this.LblRegistrar.Font = new System.Drawing.Font("Unna", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblRegistrar.Location = new System.Drawing.Point(202, 26);
-            this.LblRegistrar.Name = "LblRegistrar";
-            this.LblRegistrar.Size = new System.Drawing.Size(501, 62);
-            this.LblRegistrar.TabIndex = 9;
-            this.LblRegistrar.Text = "REGISTRAR VEHICULO";
-            this.LblRegistrar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LblRegistrarV.AutoSize = true;
+            this.LblRegistrarV.BackColor = System.Drawing.Color.Transparent;
+            this.LblRegistrarV.Font = new System.Drawing.Font("Unna", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRegistrarV.Location = new System.Drawing.Point(202, 26);
+            this.LblRegistrarV.Name = "LblRegistrarV";
+            this.LblRegistrarV.Size = new System.Drawing.Size(501, 62);
+            this.LblRegistrarV.TabIndex = 9;
+            this.LblRegistrarV.Text = "REGISTRAR VEHICULO";
+            this.LblRegistrarV.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // LblTipoV
             // 
@@ -131,12 +142,12 @@ namespace ParkingLot__PARCIAL2_
             this.LblPlacaV.Text = "PLACA VEHICULO";
             this.LblPlacaV.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox1
+            // TxtBoxPlacaV
             // 
-            this.textBox1.Location = new System.Drawing.Point(643, 119);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(222, 20);
-            this.textBox1.TabIndex = 13;
+            this.TxtBoxPlacaV.Location = new System.Drawing.Point(643, 119);
+            this.TxtBoxPlacaV.Name = "TxtBoxPlacaV";
+            this.TxtBoxPlacaV.Size = new System.Drawing.Size(222, 20);
+            this.TxtBoxPlacaV.TabIndex = 13;
             // 
             // dateTimePicker1
             // 
@@ -151,12 +162,133 @@ namespace ParkingLot__PARCIAL2_
             this.LblMarcaV.AutoSize = true;
             this.LblMarcaV.BackColor = System.Drawing.Color.Transparent;
             this.LblMarcaV.Font = new System.Drawing.Font("Unna", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblMarcaV.Location = new System.Drawing.Point(313, 186);
+            this.LblMarcaV.Location = new System.Drawing.Point(29, 163);
             this.LblMarcaV.Name = "LblMarcaV";
             this.LblMarcaV.Size = new System.Drawing.Size(186, 27);
             this.LblMarcaV.TabIndex = 15;
             this.LblMarcaV.Text = "MARCA VEHICULO";
             this.LblMarcaV.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TxtBoxMarcaV
+            // 
+            this.TxtBoxMarcaV.Location = new System.Drawing.Point(220, 165);
+            this.TxtBoxMarcaV.Name = "TxtBoxMarcaV";
+            this.TxtBoxMarcaV.Size = new System.Drawing.Size(222, 20);
+            this.TxtBoxMarcaV.TabIndex = 16;
+            // 
+            // LblNumeroI
+            // 
+            this.LblNumeroI.AutoSize = true;
+            this.LblNumeroI.BackColor = System.Drawing.Color.Transparent;
+            this.LblNumeroI.Font = new System.Drawing.Font("Unna", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNumeroI.Location = new System.Drawing.Point(29, 294);
+            this.LblNumeroI.Name = "LblNumeroI";
+            this.LblNumeroI.Size = new System.Drawing.Size(286, 27);
+            this.LblNumeroI.TabIndex = 17;
+            this.LblNumeroI.Text = "NUMERO DE IDENTIFICACIÓN";
+            this.LblNumeroI.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TxtBoxNumI
+            // 
+            this.TxtBoxNumI.Location = new System.Drawing.Point(326, 296);
+            this.TxtBoxNumI.Name = "TxtBoxNumI";
+            this.TxtBoxNumI.Size = new System.Drawing.Size(222, 20);
+            this.TxtBoxNumI.TabIndex = 18;
+            // 
+            // LblSexo
+            // 
+            this.LblSexo.AutoSize = true;
+            this.LblSexo.BackColor = System.Drawing.Color.Transparent;
+            this.LblSexo.Font = new System.Drawing.Font("Unna", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSexo.Location = new System.Drawing.Point(580, 292);
+            this.LblSexo.Name = "LblSexo";
+            this.LblSexo.Size = new System.Drawing.Size(60, 27);
+            this.LblSexo.TabIndex = 19;
+            this.LblSexo.Text = "SEXO";
+            this.LblSexo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ComBoxSexo
+            // 
+            this.ComBoxSexo.FormattingEnabled = true;
+            this.ComBoxSexo.Items.AddRange(new object[] {
+            "MASCULINO",
+            "FEMENINO",
+            "OTRO"});
+            this.ComBoxSexo.Location = new System.Drawing.Point(643, 295);
+            this.ComBoxSexo.Name = "ComBoxSexo";
+            this.ComBoxSexo.Size = new System.Drawing.Size(222, 21);
+            this.ComBoxSexo.TabIndex = 20;
+            // 
+            // LblAfiliacion
+            // 
+            this.LblAfiliacion.AutoSize = true;
+            this.LblAfiliacion.BackColor = System.Drawing.Color.Transparent;
+            this.LblAfiliacion.Font = new System.Drawing.Font("Unna", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAfiliacion.Location = new System.Drawing.Point(29, 344);
+            this.LblAfiliacion.Name = "LblAfiliacion";
+            this.LblAfiliacion.Size = new System.Drawing.Size(121, 27);
+            this.LblAfiliacion.TabIndex = 21;
+            this.LblAfiliacion.Text = "AFILIACIÓN";
+            this.LblAfiliacion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ComBoxAfiliacion
+            // 
+            this.ComBoxAfiliacion.FormattingEnabled = true;
+            this.ComBoxAfiliacion.Items.AddRange(new object[] {
+            "SI",
+            "NO"});
+            this.ComBoxAfiliacion.Location = new System.Drawing.Point(154, 346);
+            this.ComBoxAfiliacion.Name = "ComBoxAfiliacion";
+            this.ComBoxAfiliacion.Size = new System.Drawing.Size(222, 21);
+            this.ComBoxAfiliacion.TabIndex = 22;
+            // 
+            // BtnRegistrarV
+            // 
+            this.BtnRegistrarV.BackColor = System.Drawing.Color.LemonChiffon;
+            this.BtnRegistrarV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRegistrarV.Font = new System.Drawing.Font("Unna", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRegistrarV.Location = new System.Drawing.Point(133, 451);
+            this.BtnRegistrarV.Name = "BtnRegistrarV";
+            this.BtnRegistrarV.Size = new System.Drawing.Size(291, 55);
+            this.BtnRegistrarV.TabIndex = 23;
+            this.BtnRegistrarV.Text = "REGISTRAR VEHICULO";
+            this.BtnRegistrarV.UseVisualStyleBackColor = false;
+            // 
+            // BtnBorrarDatos
+            // 
+            this.BtnBorrarDatos.BackColor = System.Drawing.Color.LemonChiffon;
+            this.BtnBorrarDatos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnBorrarDatos.Font = new System.Drawing.Font("Unna", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnBorrarDatos.Location = new System.Drawing.Point(483, 451);
+            this.BtnBorrarDatos.Name = "BtnBorrarDatos";
+            this.BtnBorrarDatos.Size = new System.Drawing.Size(291, 55);
+            this.BtnBorrarDatos.TabIndex = 24;
+            this.BtnBorrarDatos.Text = "BORRAR DATOS";
+            this.BtnBorrarDatos.UseVisualStyleBackColor = false;
+            // 
+            // BtnVolver
+            // 
+            this.BtnVolver.BackColor = System.Drawing.Color.LemonChiffon;
+            this.BtnVolver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnVolver.Font = new System.Drawing.Font("Unna", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnVolver.Location = new System.Drawing.Point(299, 521);
+            this.BtnVolver.Name = "BtnVolver";
+            this.BtnVolver.Size = new System.Drawing.Size(291, 55);
+            this.BtnVolver.TabIndex = 25;
+            this.BtnVolver.Text = "VOLVER";
+            this.BtnVolver.UseVisualStyleBackColor = false;
+            // 
+            // LblRegistrarC
+            // 
+            this.LblRegistrarC.AutoSize = true;
+            this.LblRegistrarC.BackColor = System.Drawing.Color.Transparent;
+            this.LblRegistrarC.Font = new System.Drawing.Font("Unna", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRegistrarC.Location = new System.Drawing.Point(222, 213);
+            this.LblRegistrarC.Name = "LblRegistrarC";
+            this.LblRegistrarC.Size = new System.Drawing.Size(459, 62);
+            this.LblRegistrarC.TabIndex = 26;
+            this.LblRegistrarC.Text = "REGISTRAR CLIENTE";
+            this.LblRegistrarC.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Registrar
             // 
@@ -164,13 +296,24 @@ namespace ParkingLot__PARCIAL2_
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(907, 623);
+            this.Controls.Add(this.LblRegistrarC);
+            this.Controls.Add(this.BtnVolver);
+            this.Controls.Add(this.BtnBorrarDatos);
+            this.Controls.Add(this.BtnRegistrarV);
+            this.Controls.Add(this.ComBoxAfiliacion);
+            this.Controls.Add(this.LblAfiliacion);
+            this.Controls.Add(this.ComBoxSexo);
+            this.Controls.Add(this.LblSexo);
+            this.Controls.Add(this.TxtBoxNumI);
+            this.Controls.Add(this.LblNumeroI);
+            this.Controls.Add(this.TxtBoxMarcaV);
             this.Controls.Add(this.LblMarcaV);
             this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtBoxPlacaV);
             this.Controls.Add(this.LblPlacaV);
             this.Controls.Add(this.ComBoxTipoV);
             this.Controls.Add(this.LblTipoV);
-            this.Controls.Add(this.LblRegistrar);
+            this.Controls.Add(this.LblRegistrarV);
             this.Controls.Add(this.PicBoxMarco8);
             this.Controls.Add(this.PicBoxMarco7);
             this.Controls.Add(this.PicBoxMarco6);
@@ -192,12 +335,23 @@ namespace ParkingLot__PARCIAL2_
         private System.Windows.Forms.PictureBox PicBoxMarco6;
         private System.Windows.Forms.PictureBox PicBoxMarco7;
         private System.Windows.Forms.PictureBox PicBoxMarco8;
-        private System.Windows.Forms.Label LblRegistrar;
+        private System.Windows.Forms.Label LblRegistrarV;
         private System.Windows.Forms.Label LblTipoV;
         private System.Windows.Forms.ComboBox ComBoxTipoV;
         private System.Windows.Forms.Label LblPlacaV;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TxtBoxPlacaV;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label LblMarcaV;
+        private System.Windows.Forms.TextBox TxtBoxMarcaV;
+        private System.Windows.Forms.Label LblNumeroI;
+        private System.Windows.Forms.TextBox TxtBoxNumI;
+        private System.Windows.Forms.Label LblSexo;
+        private System.Windows.Forms.ComboBox ComBoxSexo;
+        private System.Windows.Forms.Label LblAfiliacion;
+        private System.Windows.Forms.ComboBox ComBoxAfiliacion;
+        private System.Windows.Forms.Button BtnRegistrarV;
+        private System.Windows.Forms.Button BtnBorrarDatos;
+        private System.Windows.Forms.Button BtnVolver;
+        private System.Windows.Forms.Label LblRegistrarC;
     }
 }
